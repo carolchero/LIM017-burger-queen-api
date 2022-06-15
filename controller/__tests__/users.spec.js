cf61eca00427264791460114d83be8a8dbde2ac8
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-06-15 14:52:30 -0500

    cambios debido a testing

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,6 +1,12 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+
 const {
   getUsers,
 } = require('../users');
+
+
 
 describe('getUsers', () => {
   it('should get users collection', (done) => {
