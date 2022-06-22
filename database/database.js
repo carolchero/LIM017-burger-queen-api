7c532e8489f0fd7e8391515d57cf796469a9be21
Shirley Aguilar <97176343+Shirley-Aguilar-Aguilar@users.noreply.github.com>
2022-06-22 18:11:26 -0500

    update db config

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 // database, username,password
 // la conexión permite crear tablas
-const postgreConnection = new Sequelize('tfflabxe', 'tfflabxe', 'PwEXVZgQwRRVHrPHFuE2eMe7y0ECgz52', {
-  host: 'castor.db.elephantsql.com', // lugar donde esta la base de datos
+const postgreConnection = new Sequelize('blhgsisp', 'blhgsisp', 'kWYdK7o_E24-qvUcKgWAdEVtZ3ntRewX', {
+  host: 'jelani.db.elephantsql.com', // lugar donde esta la base de datos
   port: 5432,
   dialect: 'postgres',
   logging: false,
