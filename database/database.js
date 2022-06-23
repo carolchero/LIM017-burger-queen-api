74ef99fcf24c5eb8b1d5970c467e550ac9a062b3
Carol Chero Paz <cheropazpilar@gmail.com>
2022-06-23 11:22:19 -0500

    cambios en mi databases

--- database/database.js
+++ database/database.js
@@ -1,15 +1,9 @@
 const Sequelize = require('sequelize');
 // database, username,password
 // la conexión permite crear tablas
-<<<<<<< HEAD
 const postgreConnection = new Sequelize('apiburguerv1', 'postgres', '123456789', {
   host: 'localhost', // lugar donde esta la base de datos
   port: 5433,
-=======
-const postgreConnection = new Sequelize('apiburguerv1', 'postgres', '123456', {
-  host: 'localhost', // lugar donde esta la base de datos
-  port: 5432,
->>>>>>> 7cff662660565403b2612f4dd13b71a3c3372a9b
   dialect: 'postgres',
   logging: false,
 });
