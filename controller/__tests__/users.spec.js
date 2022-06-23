4c632523999a575e34f2f03b6c28ffcf06f4173d
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-06-23 14:16:46 -0500

    testing de autenticación al 100%

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -11,7 +11,13 @@ const adminUser = {
   password: '123456',
 };
 
-describe('getUsers', () => {
+const userNoAdmi = {
+  email: 'user5@gmail.com',
+  password: '123456',
+  roles: false,
+};
+
+describe('GET USERS', () => {
   it('should get users collection', (done) => {
     request.post('/auth').send(adminUser).then((resp) => {
       const token = resp.body.accessToken;
@@ -19,23 +25,51 @@ describe('getUsers', () => {
         .expect('Content-Type', /json/)
         .expect(200)
         .then((response) => {
-          console.log(response.body);
           expect(Array.isArray(response.body)).toBeTruthy();
+          done();
+        });
+    });
+  });
+  it('should return an error 401 when is not token', (done) => {
+    request.post('/auth').send({}).then((resp) => {
+      const token = resp.body;
+      request.get('/users').set('access-token', `${token}`)
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then((response) => {
+          expect(response.body.message).toEqual('Unauthorized');
           done();
         });
     });
   });
 });
 
-it('should return an error 401 when is not token', (done) => {
-  request.post('/auth').send({}).then((resp) => {
-    const token = resp.body;
-    request.get('/users').set('access-token', `${token}`)
+// en proceso
+/* describe('DELETE USER', () => {
+  it('should delete user by id', (done) => {
+    request.post('/users/8')
+      .send(userNoAdmi)
       .expect('Content-Type', /json/)
-      .expect(401)
+      .expect(200);
       .then((response) => {
-        expect(response.body.message).toEqual('Unauthorized');
-        done();
+        console.log(response.body)
+        expect(response.statusCode).toEqual(200);
       });
+    done();
   });
 });
+
+describe('POST USERS', () => {
+  it('should', (done) => {
+    request.post('/users')
+      .send(userNoAdmi)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        console.log(response.body)
+        expect(response.statusCode).toEqual(200);
+      });
+    done();
+  });
+});
+
+ */
