8757b7a262faf5bfc89d03cdd1bbe172037b404f
Carol Chero Paz <cheropazpilar@gmail.com>
2022-06-23 08:53:47 -0500

    cmabios en mi config,database,modelSheme

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'postgresql://localhost:5432/test';
+exports.dbUrl = process.env.DB_URL || 'postgresql://localhost:5433/test';
 exports.secret = process.env.JWT_SECRET || 'BURGUERA12022LABORATORIA';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admiDefault@gmail.com';
 exports.access_token_life_in_seconds = 10080; // 7 dias
-exports.adminPassword = process.env.ADMIN_PASSWORD || '123456';
+exports.adminPassword = process.env.ADMIN_PASSWORD || '123456789';
